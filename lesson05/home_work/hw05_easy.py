97817b12d64d4cd9811f4affe887df061fb1ecdf
maksyutovrv <maksyutovrv@ya.ru>
2019-02-08 01:08:19 +0300

    Lesson 5 easy& normal level tasks are completed

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,74 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+print('*****TASK 1*****', '\n')
+
+
+def new_dir(dir_name):
+    try:
+        os.mkdir(dir_name)
+    except Exception:
+        print('Something got wrong with  ' + dir_name)
+
+
+def new_dirs():
+    for i in range(1,10):
+        new_dir('dir_' + str(i))
+
+
+print(os.listdir("."))
+
+
+def delete_dir(dir):
+    try:
+        os.rmdir(dir)
+    except Exception:
+        print('Something wrong with remove ' + dir)
+
+
+def delete_dirs():
+    for i in range(1, 10):
+        delete_dir('dir_' + str(i))
+
+
+new_dirs()
+print(os.listdir("."), '\n')
+delete_dirs()
+
+print('*****END OF TASK 1*****', '\n')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+print('*****TASK 2*****', '\n')
+
+
+def show_dirs():
+    for item in os.listdir():
+        if os.path.isdir(item):
+            print(item)
+
+new_dirs()
+show_dirs()
+delete_dirs()
+
+print('\n', '*****END OF TASK 2*****', '\n')
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+print('*****TASK 3*****', '\n')
+print(os.listdir("."), '\n')
+
+
+def copy_file(source, loc):
+    with open(source, 'r', encoding="utf-8") as src, open(loc, 'w', encoding="utf-8") as dst: dst.write(src.read())
+
+
+copy_file(os.path.basename(__file__), os.path.basename(__file__) + '.copy')
+
+print(os.listdir("."), '\n')
+print('\n', '*****END OF TASK 3*****', '\n')
+
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,46 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import hw05_easy as my_funcs
+
+menu = [
+    '1. Перейти в папку',
+    '2. Просмотреть содержимое текущей папки',
+    '3. Удалить папку',
+    '4. Создать папку',
+]
+
+while(True):
+    for item in menu:
+        print(item)
+
+    try:
+        selected = int(input())
+    except Exception:
+        print('Good Bay!')
+        break
+
+    if selected == 1 or selected == 3 or selected == 4:
+        print('Input dir name: ')
+        dir_name = input()
+
+    if selected == 1:
+        try:
+            os.chdir(dir_name)
+            print('You have changed location successfully')
+        except Exception as e:
+            print('Something wrong with chdir ' + str(dir_name) + ': ' + str(e))
+    elif selected == 2:
+        my_funcs.show_dirs()
+    elif selected == 3:
+        my_funcs.delete_dir(dir_name)
+        print('Folder Deleted')
+    elif selected == 4:
+        my_funcs.new_dir(dir_name)
+        print('Folder created')
+
+    else:
+        print('Try once more')
+
