73ac4f1e01d1d86504220d88b72eb577d6862356
maksyutovrv <maksyutovrv@ya.ru>
2019-02-12 00:16:20 +0300

    Lesson 6 EASY homework completed

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,61 @@
-# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
-# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+# Задача - 1
+# Опишите несколько классов TownCar, SportCar, WorkCar, PoliceCar
+# У каждого класса должны быть следующие аттрибуты:
+# speed, color, name, is_police - Булево значение.
+# А так же несколько методов: go, stop, turn(direction) - которые должны сообщать,
+#  о том что машина поехала, остановилась, повернула(куда)
+
+# Задача - 2
+# Посмотрите на задачу-1 подумайте как выделить общие признаки классов
+
+class TownCar:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def car_go(self):
+        print('Машина {} начала свое движение!'.format(self.name))
+
+    def car_stop(self):
+        print('Машина {} остановилась!'.format(self.name))
+
+    def car_turn(self, direction):
+        print('Машина {} повернула {}!'.format(self.name, direction))
+
+    def go(self):
+        print('Машина {} поехала!'.format(self.name))
+
+    def stop(self):
+        print('Машина {} остановилась!'.format(self.name))
+
+    def turn(self, direction):
+        print('Машина {} повернула {}!'.format(self.name, direction))
 
 
-# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
-# Предусмотреть в классе методы:
-# проверка, является ли фигура равнобочной трапецией;
-# вычисления: длины сторон, периметр, площадь.
+class SportCar(TownCar):
+    def __init__(self, speed, color, name, is_police=False):
+        TownCar.__init__(self, speed, color, name, is_police=False)
 
+
+
+class WorkCar(TownCar):
+    def __init__(self, speed, color, name, is_police=False):
+        TownCar.__init__(self, speed, color, name, is_police=False)
+
+
+class PoliceCar(TownCar):
+    def __init__(self, speed, color, name, is_police=False):
+        TownCar.__init__(self, speed, color, name, is_police=True)
+
+
+#car1 = TownCar(100, 'red', 'Honda')
+#car2 = SportCar(100, 'red', 'Honda')
+#car2.turn('назад')
+
+my_car = TownCar(100, 'Blue', 'TESLA')
+
+my_car.car_go()
+my_car.car_turn('Налево')
+my_car.car_stop()
